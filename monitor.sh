db10156b05dc0d649c568e3182fe3e472bb3f66b
Ananth <anantrao@amazon.com>
2017-09-10 23:17:12 +0530

    More changes to monitor.

--- monitor.sh
+++ monitor.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 while  true
 do
-result=$(ps aux | grep -c  "[python /home/anantrao/workspace/praw/rickandmortybot/causeWhyNotMate/causeWhyNotMaty.py]python /home/anantrao/workspace/praw/causeWhyNotMate/causeWhyNotMate/causeWhyNotMaty.py")
+result=$(ps aux | grep -c  "[python /home/anantrao/workspace/praw/rickandmortybot/causeWhyNotMate/causeWhyNotMaty.py]python /home/anantrao/workspace/praw/rickandmortybot/causeWhyNotMate/causeWhyNotMaty.py")
 case $result in
         0) echo "restarting python causeWhyNotMaty.py"
            python /home/anantrao/workspace/praw/rickandmortybot/causeWhyNotMate/causeWhyNotMaty.py &

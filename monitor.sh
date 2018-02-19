ee6c07fa86b9abcc49ff1759307ef3e3f9a7dd51
Ananth <anantrao@amazon.com>
2018-02-19 23:15:10 +0530

    Updating path

--- monitor.sh
+++ monitor.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 while  true
 do
-result=$(ps aux | grep -c  "[python /home/anantrao/workspace/praw/causeWhyNotMate/canYouProveYouAre/causeWhyNotMaty.py]python /home/anantrao/workspace/praw/causeWhyNotMate/canYouProveYouAre/causeWhyNotMaty.py")
+result=$(ps aux | grep -c  "[python //home/anantrao/workspace/praw/canYouProveYouAre/causeWhyNotMate/causeWhyNotMaty.py]python /home/anantrao/workspace/praw/canYouProveYouAre/causeWhyNotMate/causeWhyNotMaty.py")
 case $result in
         0) echo "restarting python causeWhyNotMaty.py"
-           python /home/anantrao/workspace/praw/causeWhyNotMate/canYouProveYouAre/causeWhyNotMaty.py &
+           python /home/anantrao/workspace/praw/canYouProveYouAre/causeWhyNotMate/causeWhyNotMaty.py &
            sleep 60
                 ;;
         1)
